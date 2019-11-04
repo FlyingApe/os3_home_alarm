bef3cb171b80e29a26369eda968a7794c5054ab0
Unknown <melvinkusters0@gmail.com>
2019-11-04 19:54:42 +0100

    Base project for server added.

---
+++ server/src/test/java/com/os3alarm/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.os3alarm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

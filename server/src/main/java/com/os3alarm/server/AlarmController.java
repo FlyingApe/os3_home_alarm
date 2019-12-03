452bc6aee48a4b89abc8be2b924146eb26079560
Lu <l.j.hovestadt@gmail.com>
2019-12-03 11:06:13 +0100

    SpringSocket first prototype

--- server/src/main/java/com/os3alarm/server/AlarmController.java
+++ server/src/main/java/com/os3alarm/server/AlarmController.java
@@ -5,6 +5,12 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -61,6 +67,14 @@ public class AlarmController {
             return "";
         });
     }
+
+
+
+    //@MessageMapping("/hello")
+    //@SendTo("topic/greetings")
+    //public Alarm sendAlarmToClient(Alarm alarm) throws Exception {
+    //    return alarm;
+    //}
 
 
 
---
+++ server/src/main/java/com/os3alarm/server/Greeting.java
@@ -0,0 +1,16 @@
+package com.os3alarm.server;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {}
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}

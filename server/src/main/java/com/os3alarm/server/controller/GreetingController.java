696723deda6b2de65bf65cc86a9dcd4796fb9dc6
Lu <L.j.hovestadt@gmail.com>
2019-12-07 14:26:25 +0100

    integration of sample project into main project

---
+++ server/src/main/java/com/os3alarm/server/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.os3alarm.server.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.os3alarm.server.model.Greeting;
+import com.os3alarm.server.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
---
+++ server/src/main/java/com/os3alarm/server/model/Greeting.java
@@ -0,0 +1,18 @@
+package com.os3alarm.server.model;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}

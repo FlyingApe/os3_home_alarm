d963ede312d1c6b1e9bbe3dc9d0ff358fafc431c
Seraphix <melvinkusters0@gmail.com>
2019-12-10 14:06:39 +0100

    [Spring] Refactored RelayData and implemented pushToSocket().

--- server/src/main/java/com/os3alarm/server/models/RelayDataObserver.java
+++ server/src/main/java/com/os3alarm/server/models/RelayDataObserver.java
@@ -1,23 +1,19 @@
 package com.os3alarm.server.models;
 
+import org.springframework.messaging.handler.annotation.SendTo;
+
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-public class RelayDataObserver implements PropertyChangeListener, RelayDataReceiver {
-    private String received;
+public class RelayDataObserver implements PropertyChangeListener {
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-        this.setReceived((String) (evt.getNewValue()));
+        this.pushToSocket((String) (evt.getNewValue()));
     }
 
-    @Override
-    public String getReceived() {
-        return received;
-    }
-
-    @Override
-    public void setReceived(String received) {
-        this.received = received;
+    @SendTo("/alarm/sensordata")
+    private String pushToSocket(String message) {
+        return message;
     }
 }
--- server/src/main/java/com/os3alarm/server/models/RelayDataReceiver.java
+++
@@ -1,6 +0,0 @@
-package com.os3alarm.server.models;
-
-public interface RelayDataReceiver {
-    String getReceived();
-    void setReceived(String received);
-}

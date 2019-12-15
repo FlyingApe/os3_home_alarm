5f6b7f7f1b2265083fbb5ad69c3eafa9ee9488dd
Lu <L.j.hovestadt@gmail.com>
2019-12-15 22:54:59 +0100

    [Security] Overridden http security within config

--- server/src/main/java/com/os3alarm/server/config/SecurityConfig.java
+++ server/src/main/java/com/os3alarm/server/config/SecurityConfig.java
@@ -16,4 +16,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().withUser("test").password("123").roles("USER");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }

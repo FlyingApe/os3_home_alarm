1b46c037f67767839e19b2311f29cc1719a8d92e
Seraphix <melvinkusters0@gmail.com>
2020-01-16 15:03:37 +0100

    [Angular] Added Material dependencies.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,29 +9,38 @@ import {MatCardModule, MatFormFieldModule, MatListModule, MatSelectModule, MatTa
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import { AlarmDialogComponent } from './alarm-dialog/alarm-dialog.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlarmComponent,
     AlarmDetailComponent,
+    AlarmDialogComponent,
   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        BrowserAnimationsModule,
-        MatTabsModule,
-        MatTableModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        FormsModule,
-        RouterModule.forRoot([
-            {path: 'alarm', component: AlarmComponent},
-            {path: 'alarm/:id', component: AlarmDetailComponent}
-        ]),
-        MatListModule,
-    ],
+  imports: [
+      BrowserModule,
+      HttpClientModule,
+      BrowserAnimationsModule,
+      MatTabsModule,
+      MatTableModule,
+      MatCardModule,
+      MatFormFieldModule,
+      MatSelectModule,
+      FormsModule,
+      RouterModule.forRoot([
+          {path: 'alarm', component: AlarmComponent},
+          {path: 'alarm/:id', component: AlarmDetailComponent}
+      ]),
+      MatListModule,
+      MatButtonModule,
+      MatDialogModule,
+      MatInputModule,
+  ],
+  entryComponents: [AlarmDialogComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
